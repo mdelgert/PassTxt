c912ef2a20e0a44a1181f87bba1d67ffedc3f913
Matthew Elgert <mdelgert@yahoo.com>
2025-03-24 18:41:30 -0400

    Update base path in Vite config to reflect project name change to PassTxt

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ImprovWiFiWeb/' : '/',
+  base: mode === 'production' ? '/PassTxt/' : '/',
   build: {
     rollupOptions: {
       input: {
